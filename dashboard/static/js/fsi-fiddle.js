e79a7c4c2e0492a09dbeefc81b21f9dff0a4ad10
zpurcey <github@purcellshouse.com>
2018-06-24 14:31:17 +0000

    Adding dashboard fiddle to change tickets to transactions

---
+++ dashboard/static/js/fsi-fiddle.js
@@ -0,0 +1,10 @@
+// Replace labels to mock dashboard for fs vertical cusotmers
+var originalLabel = 'TICKETS SOLD';
+var newLabel = 'TRANSACTIONS';
+var labels = document.getElementsByClassName("type-label");
+if (labels.length && labels.length > 0) {
+    for(var i = 0; i < labels.length; i++)
+    {
+        labels[i].innerText = labels[i].innerText.replace(originalLabel, newLabel);
+    }
+}
